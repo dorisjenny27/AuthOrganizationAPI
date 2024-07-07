ffaac098037275d14dddd09e7296268ceb4e416b
dorisjenny27 <dorisjenny27@gmail.com>
2024-07-07 22:02:11 +0100

    Update on Authentication Organization

--- AuthOrganizationAPI/obj/Debug/net8.0/AuthOrganizationAPI.AssemblyInfo.cs
+++ AuthOrganizationAPI/obj/Debug/net8.0/AuthOrganizationAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthOrganizationAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e82456148bcd922826c21ca9188daf65b1160b0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+244fe4c3ad3dad2c38f582946f2ba44bb327f003")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthOrganizationAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthOrganizationAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
