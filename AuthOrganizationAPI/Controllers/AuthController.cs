d1502a95333cb477689ef691b08fbf3b77318d31
dorisjenny27 <dorisjenny27@gmail.com>
2024-07-09 00:47:11 +0100

    Feat: Update on Auth organizatiuons

--- AuthOrganizationAPI/Controllers/AuthController.cs
+++ AuthOrganizationAPI/Controllers/AuthController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AuthOrganizationAPI.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class AuthController : ControllerBase
     {
